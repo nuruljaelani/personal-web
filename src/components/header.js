e9d2a4f4f292524654eeec135fcc0cb9aecb6137
nuruljaelani <nuruljaelani96@gmail.com>
2021-08-28 11:27:11 +0700

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,29 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div className="bg-hero h-[700px]">
+      <div className="container mx-auto">
+        <div className="flex items-center">
+          <div className="w-3/12 uppercase text-xl text-white font-semibold">Personal Web</div>
+          <div className="w-6/12">
+            <ul className="flex justify-center items-center my-auto space-x-10">
+              <li className="text-white font-semibold text-lg opacity-70"><a>Profile</a></li>
+              <li className="text-white font-semibold text-lg opacity-70"><a>Skills</a></li>
+              <li className="text-white font-semibold text-lg opacity-70"><a>Projects</a></li>
+              <li className="text-white font-semibold text-lg opacity-70"><a>Contact</a></li>
+            </ul>
+          </div>
+          <div className="w-3/12 flex justify-end">
+            <button className="text-white border border-white rounded-3xl py-3 px-4">Contact</button>
+          </div>
+        </div>
+        <div className="text-center font-mono">
+          <p className="text-3xl font-semibold my-6">Saya seorang front-end web developer, back-end web developer</p>
+          <p className="text-lg">Memulai programing sejak tahun 2019</p>
+        </div>
+        <button className="rounded-3xl border border-white py-3 px-4">Pelajari</button>
+      </div>
     </div>
   </header>
 )
@@ -36,7 +35,7 @@ Header.propTypes = {
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: `Home`,
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import "@fontsource/ibm-plex-mono" // Defaults to weight 400.
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,13 +27,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
         <main>{children}</main>
         <footer
           style={{
@@ -43,7 +37,6 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
     </>
   )
 }
